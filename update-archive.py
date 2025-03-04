e8bf68ad11098867c7575ae41d1b630b4906f188
Alexey Fedoseev <aleksey@fedoseev.net>
2025-03-04 19:44:27 +0300

    correct unit info unpacking

--- update-archive.py
+++ update-archive.py
@@ -46,7 +46,7 @@ def download_artefact(player_id, artefact):
     if not os.path.isdir(player_dir):
         os.mkdir(player_dir)
 
-    artefact_id, checksum = artefact 
+    artefact_id, checksum, _, _ = artefact 
 
     if checksum not in Hashes:
         Hashes[checksum] = (player_id, artefact_id)
