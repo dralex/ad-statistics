f890ebb718de86cfbea01d0d840f0b43bd614361
Alexey Fedoseev <aleksey@fedoseev.net>
2025-12-23 11:38:26 +0300

    sync srcipt for Arena assets

---
+++ sync-assets.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+# -----------------------------------------------------------------------------
+#  The Berloga Apiary Defence statistics analysis tool
+# 
+#  Arena roster files synchronizer
+# 
+#  Copyright (C) 2025 Alexey Fedoseev <aleksey@fedoseev.net>
+# 
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+# 
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+# 
+#  You should have received a copy of the GNU General Public License
+#  along with this program. If not, see https://www.gnu.org/licenses/
+#  ----------------------------------------------------------------------------- 
+
+import os
+import shutil
+
+SOURCE_DIR = 'remote-s3/talent'
+ROSTER_TARGET_DIR = 'arena-rosters'
+PROGRAMS_TARGET_DIR = 'arena-programs'
+
+ROSTER_EXT = '.zip'
+PROGRAM_EXT = '.graphml'
+
+if __name__ == '__main__':
+
+    total_programs = total_rosters = 0
+    copied_programs = copied_rosters = 0 
+    
+    for f in os.listdir(SOURCE_DIR):
+        path = os.path.join(SOURCE_DIR, f)
+        if f.find(PROGRAM_EXT) > 0:
+            total_programs += 1
+            target = os.path.join(PROGRAMS_TARGET_DIR)
+            if !os.path.exists(target):
+                sutils.copyfile(path, target)
+                print(f)
+                copied_programs += 1
+        elif f.find(ROSTER_EXT) > 0:
+            total_rosters += 1
+
+    print('Summary:')
+    print('Programs: {} / {}'.format(copied_programs, total_programs))
+    print('Rosters: {} / {}'.format(copied_rosters, total_rosters))
